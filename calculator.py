011ad9676740d38d5b739bee96eba847abc9b413
Sumaya531 <sumayaarpita5@gmail.com>
2026-06-24 02:44:27 +0600

    Fix power function indentation

--- calculator.py
+++ calculator.py
@@ -13,8 +13,8 @@ def divide(a, b):
     return a / b
   
 def power(base, exponent):
-    """Return base raised to the given exponent."""
-return base ** exponent
+    """Return base raised to the given exponent.""" 
+ return base ** exponent
 def modulo(a, b):
     """Return the remainder of a divided by b."""
     if b == 0:
