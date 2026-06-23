bce5027ce69307517fd327a787f62f98d449a4d8
Sumaya531 <sumayaarpita5@gmail.com>
2026-06-24 02:51:39 +0600

    Add modulo operation to calculator (#2)
    * Add modulo operation (Closes #1)
    
    * Fix power function indentation

--- calculator.py
+++ calculator.py
@@ -13,5 +13,10 @@ def divide(a, b):
     return a / b
   
 def power(base, exponent):
-    """Return base raised to the given exponent."""
-return base ** exponent
+    """Return base raised to the given exponent.""" 
+ return base ** exponent
+def modulo(a, b):
+    """Return the remainder of a divided by b."""
+    if b == 0:
+        raise ValueError("Cannot take modulo by zero")
+    return a % b
